73d66a7e8410a37298fd49b93cee844a15ed50be
Sadra Abbaszadeh <abbsadra@gmail.com>
2026-04-27 23:39:16 +0330

    تغییرات در ترتیب بندی کد و اضافه شدن دکمه لیست تیکت

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 TOKEN = "YOUR-TOKEN"
 ADMIN_ID = "YOUR-ADMIN-ID"
+ADMIN_USER_NAME = "YOUR-ADMIN-USERNAME"
 commands_list = [
         {"command": "start", "description": "شروع به کار با ربات"},
         {"command": "help", "description": "نمایش راهنمای دستورات"},
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from rubka import Robot, Message
 from rubka.button import ChatKeypadBuilder,InlineBuilder
 from answer_list import start_message , about_me
-from config import TOKEN,ADMIN_ID,commands_list
+from config import TOKEN,ADMIN_ID,ADMIN_USER_NAME,commands_list
 
 # مقداردهی اولیه ربات با استفاده از توکن
 bot = Robot(TOKEN)
 #آیدی عددی ادمین
 admin_id = ADMIN_ID
-
+admin_user_name = ADMIN_USER_NAME
 
 # تعریف کی‌پد (صفحه کلید) اصلی ربات با دکمه‌های کاربردی
 # این کی‌پد برای تعاملات عمومی کاربران استفاده می‌شود
@@ -30,7 +30,8 @@ admin_panel_keypad = (
     ChatKeypadBuilder()
     .row(
         ChatKeypadBuilder().button(id="bot_status",text="خاموش/روشن"),
-        ChatKeypadBuilder().button(id="amar_bot",text="📊 آمار ربات")
+        ChatKeypadBuilder().button(id="amar_bot",text="📊 آمار ربات"),
+        ChatKeypadBuilder().button(id="list_ticket",text="لیست تیکت")
     )
     .build()
 )
@@ -75,6 +76,8 @@ support_button_keypad = (
     )
     .build()
 )
+
+#-------------MAIN-MENU-BOT---------#
 @bot.on_message(commands=['start'])
 async def welcome_message(bot: Robot, message: Message):
     print(f"پیام جدید: {message.text}")
@@ -82,6 +85,34 @@ async def welcome_message(bot: Robot, message: Message):
     await bot.set_commands(commands_list)
 
 
+
+@bot.callback_query("about_me")
+async def about_bot(bot:Robot,message:Message):
+    await message.answer(about_me())
+
+@bot.callback_query("support_menu")
+async def contact_support(bot:Robot,message:Message):
+    await message.reply(
+        "کدام یک از روش های زیر را ترجیح میدهید برای ارتباط؟",
+        chat_keypad = support_button_keypad
+        )
+
+
+@bot.callback_query(button_id="support_id")
+async def support_pv(bot:Robot,message:Message):
+    await message.reply(f"برای ارسال پیام به پشتیبانی با آیدی زیر در ارتباط باشید \n {admin_user_name}")
+
+
+
+@bot.callback_query("submit_ticket")
+async def submit_ticket(bot:Robot,message:Message):
+    await message.reply(
+        "لطفا پیامی را که میخواهید به ادمین ارسال شود را بفرستید"
+    )
+    
+
+
+#----------------ADMIN-PANEL----------------#
 # تعریف رویداد برای دستور '/panel'
 # این تابع دسترسی به پنل ادمین را کنترل می‌کند
 @bot.on_message(commands=['panel'])
@@ -96,29 +127,8 @@ async def admin_panel(bot:Robot,message:Message):
         # اگر کاربر ادمین نبود، پیام عدم دسترسی را ارسال می‌کند
         await message.reply("شما ادمین نیستید")
 
-@bot.callback_query("about_me")
-async def about_bot(bot:Robot,message:Message):
-    await message.answer(about_me())
 
-@bot.callback_query("support_menu")
-async def contact_support(bot:Robot,message:Message):
-    await message.reply(
-        "کدام یک از روش های زیر را ترجیح میدهید برای ارتباط؟",
-        chat_keypad = support_button_keypad
-        )
-
-
-
-@bot.callback_query("submit_ticket")
-async def submit_ticket(bot:Robot,message:Message):
-    await message.reply(
-        "لطفا پیامی را که میخواهید به ادمین ارسال شود را بفرستید"
-    )
-    
-
-
-
-
+#--------------------CREATOR-COMMAND--------#
 @bot.on_message(commands=['creator', 'dev', 'admin_info', 'developer'])
 async def show_creator_info(bot: Robot, message: Message):
     await message.reply("این ربات توسط [yourname] توسعه داده شده است.\nآیدی روبیکا: @your_user_name\nگیت‌هاب: https://github.com/your_github")
